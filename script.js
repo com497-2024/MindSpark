81b50100bbe247637fc4ad1da8c6807ea5890298
Tierra Doughty <77511842+TIERRA-Z@users.noreply.github.com>
2024-04-26 09:12:53 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    var typed2 = new Typed(".typing", {
-        strings: ["Efficient Learning", "the #1 Student Resource", "the Future of Education", "MindSpark"],
-        typeSpeed: 120,
-        backSpeed: 60,
-        loop: true
-    });
-})
